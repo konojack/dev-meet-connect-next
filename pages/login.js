ed6b1e90a6d6c05b8899dca294ebeca00204f22c
konojack <konojack@gmail.com>
2022-11-13 15:45:30 +0100

    NextAuth GitHub intergation next part

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,25 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function Login() {
+  const [signing, setSignin] = useState(false);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  const handleGitHubLogin = async (e) => {
+    e.preventDefault();
+    setSignin(true);
+    await signIn('github');
+  };
+
+  useEffect(() => {
+    if (session && status !== 'loading') {
+      router.push('/');
+    }
+  }, [session, status]);
+
   return (
     <div>
       <section className="h-screen py-10 lg:py-20 bg-green-600">
@@ -18,14 +37,15 @@ export default function Login() {
               </div>
               <form action="">
                 <div className="text-center">
-                  <a
-                    className="mt-8 mb-4 p-4 flex justify-center items-center border rounded hover:bg-gray-50"
-                    href="/">
+                  <button
+                    className="mt-8 mb-4 mx-auto p-4 flex justify-center items-center border rounded hover:bg-gray-50"
+                    onClick={handleGitHubLogin}>
                     <img className="mr-4 w-6" src="/github.svg" alt="" />
                     <span className="text-xs text-gray-500 font-bold">
-                      Sign In with your GitHub
+                      {signing && 'Signing in...'}
+                      {!signing && 'Sign In with your GitHub'}
                     </span>
-                  </a>
+                  </button>
                 </div>
               </form>
             </div>
