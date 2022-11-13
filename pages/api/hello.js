360f96c0d007c43a8833db1d77cd6379c0e4e8ae
konojack <konojack@gmail.com>
2022-11-13 16:13:27 +0100

    Prisma model and testing CRUDs

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.statusCode = 200;
-  res.json({ name: 'John Doe' });
+import { user } from 'models';
+export default async (req, res) => {
+  try {
+    await user.create({
+      data: {
+        email: 'dupadupa@op.pl',
+        name: 'Andrzej',
+        image: 'huhu.jpg'
+      }
+    });
+    const allUsers = await user.findMany();
+    res.statusCode = 200;
+    res.json({ allUsers });
+  } catch (err) {
+    res.json({ err, status: 'sth wrong' });
+  }
 };
