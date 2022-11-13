93cd6cc3720f9b66080f2d89450f58920e4776db
konojack <konojack@gmail.com>
2022-11-13 15:00:34 +0100

    NextAuth Github intergration

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import GitHubProvider from 'next-auth/providers/github';
+import NextAuth from 'next-auth';
+import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ],
+  adapter: PrismaAdapter(prisma)
+});
